f5abbe7abe885c62c63f4173598a5f8b62529da8
andervyd <andervyd@rambler.ru>
2020-04-30 18:16:05 +0300

    Work pattern for JavaFX11 with settings on Maven

---
+++ src/main/java/by/andervyd/Controller.java
@@ -0,0 +1,4 @@
+package by.andervyd;
+
+public class Controller {
+}
---
+++ src/main/java/by/andervyd/MainApp.java
@@ -0,0 +1,23 @@
+package by.andervyd;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/setting.fxml"));
+        primaryStage.setTitle("__name__");
+        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
